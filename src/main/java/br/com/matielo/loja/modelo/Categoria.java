f03e13b19db6ee28ee4279474d7dbd13d16af84c
Matielo J Geronimo <matielo.dev@gmail.com>
2023-02-10 10:17:21 -0300

    atributo com tipo Enum /@Enumerated(EnumType.STRING)

---
+++ src/main/java/br/com/matielo/loja/modelo/Categoria.java
@@ -0,0 +1,6 @@
+package br.com.matielo.loja.modelo;
+
+public enum Categoria {
+
+	CELULARES, INFORMATICA, LIVROS
+}
--- src/main/java/br/com/matielo/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/matielo/loja/testes/CadastroDeProduto.java
@@ -3,20 +3,16 @@ package br.com.matielo.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.matielo.loja.dao.ProdutoDao;
+import br.com.matielo.loja.modelo.Categoria;
 import br.com.matielo.loja.modelo.Produto;
 import br.com.matielo.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi Redmi");
-		celular.setDescricao("SnapDragon");
-		celular.setPreco(new BigDecimal("800.50"));
-
+		Produto celular = new Produto("Xiaomi Redmi", "SnapDragon", new BigDecimal("800.50"), Categoria.CELULARES);
+		// parei no 5:33
 		EntityManager em = JPAUtil.getEntityManager();
 
 		ProdutoDao dao = new ProdutoDao(em);
